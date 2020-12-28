3684ab3ada353eb9b7646da29ecbca4556a44c29
RuslanPeka <pekaruslan2009@ya.ru>
2020-12-28 21:01:34 +0200

    v10.2

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
     // ========== Проект в MVC-формате ==========
     use Core\Router;
     use Core\MyHelp;
-    use Components\Orm\Connector;
     require_once "vendor/autoload.php";
 
     $router = new Router();
